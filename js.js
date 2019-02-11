1965d2e7ba235cc542c713f4dcf6f6b34aca9a40
Nikita Derevskov <42345473+NikitaDerevskov@users.noreply.github.com>
2019-02-12 00:51:00 +0500

    First Commit

---
+++ js.js
@@ -0,0 +1,33 @@
+let timeEnd = 0;
+function numberDefine(a) {
+   let name = 'sec' + a;
+   timeEnd = document.getElementById(name).innerHTML;
+   startTimer(timeEnd);
+}
+
+function startTimer(timeEnd) {
+    function autTimer(){
+        let timeEndNumber = parseInt(timeEnd);
+          for ( i = timeEndNumber; i > 0; i--) {
+
+            setTimeout(function(i) {
+                return function() {
+                    document.getElementById("endTimer").innerHTML= timeEndNumber - i;
+                };
+            }(i), 60*1000*(i)); //переписать чтобы не надо было вычитать.
+
+
+   }
+
+setTimeout(playMusic,timeEndNumber*1000*60)
+        }
+
+    autTimer();
+    function playMusic() {
+        const audio = new Audio();
+        audio.src = 'n.mp3';
+        audio.autoplay = true;
+    }
+
+
+}
